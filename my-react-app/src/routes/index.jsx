d930b17f0aece20c4d7f165fbac54a86adb5564c
goptun <ramos.matheus@icloud.com>
2023-08-01 19:15:05 -0300

    Ajuste no index.jsx

--- my-react-app/src/routes/index.jsx
+++ my-react-app/src/routes/index.jsx
@@ -1,5 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/home";
+import {
+  BrowserRouter, //é a função raiz da lib. Todas as rotas precisam estar declaradas dentro desse escopo, ou seja, nenhuma outra rota poderá ser acessada se não tiver sido declarada aqui. Lembrando que há apenas uma declaração de BrowserRouter no projeto.
+  Routes, //é a função gerenciadora de rotas, podendo existir mais de uma na aplicação.
+  Route, //é a função declarada das rotas, nela declararemos nossos componentes em padrões de páginas, podendo especificar informações como o caminho da rota entre outras informações relevantes.
+} from "react-router-dom";
+import { Home } from "../pages/home";
+import { NotFound } from "../pages/404";
+
 function RoutesApp() {
   return (
     <BrowserRouter>
@@ -10,4 +16,5 @@ function RoutesApp() {
     </BrowserRouter>
   );
 }
+
 export { RoutesApp };
